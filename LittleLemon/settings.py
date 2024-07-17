49e262cbb59925021bbd6eac8e6abf2abd766f27
Favour Adebose <favouradebose@gmail.com>
2024-07-17 02:03:41 +0100

    final commit

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -98,6 +98,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 
+DJOSER={"USER_ID_FIELD":"username"}
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from restaurant.models import Menu, Booking
-from rest_framework.authtoken.models import Token
+# from rest_framework.authtoken.models import Token
 
 # Register your models here
 admin.site.register(Menu)
 admin.site.register(Booking)
-admin.site.register(Token)
 
